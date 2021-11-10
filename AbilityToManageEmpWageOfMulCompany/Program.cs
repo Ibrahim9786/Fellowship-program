99f45be3afac8faceba09e75b384fab1a15f5604
Ibrahim9786 <sharuibrahim97@gmail.com>
2021-11-10 18:13:45 +0530

    AbilityToManageEmpWageOfMulCompany

---
+++ AbilityToManageEmpWageOfMulCompany/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AbilityToManageEmpWageOfMulCompany
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EmpWageBuilderArray empWageBuilder = new EmpWageBuilderArray();
+            empWageBuilder.addCompanyWage("DMart", 20, 2, 10);
+            empWageBuilder.addCompanyWage("Reliance", 10, 4, 20);
+            empWageBuilder.computeEmpWage();
+
+            Console.ReadKey();
+        }
+    }
+}
