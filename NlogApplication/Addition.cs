d19325c2799ed57891574a79dc216876380ac43c
Ibrahim9786 <sharuibrahim97@gmail.com>
2021-11-16 12:37:45 +0530

    NlogApplication

---
+++ NlogApplication/Addition.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NlogApplication
+{
+    class Addition
+    {
+        static Nlog nlog = new Nlog();
+
+        public static int Add(int firstNum, int secondNum)
+        {
+            int sum = 0;
+            try
+            {
+
+                if (firstNum == 0 || secondNum == 0)
+                {
+                    nlog.LogDebug("debug successfully");
+                    nlog.LogError("first Number and Second Number is zero");
+                    nlog.LogInfo("Add method");
+                    return 0;
+                }
+                sum = firstNum + secondNum;
+                nlog.LogInfo("Addition method executed");
+                nlog.LogDebug("Debug Successfully");
+                nlog.LogWarn("First Number and Second number is not zero");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            return sum;
+        }
+    }
+}
---
+++ NlogApplication/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace NlogApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Nlog");
+            Addition addition = new Addition();
+            Addition.Add(20, 55);
+            Console.ReadKey();
+        }
+    }
+}
