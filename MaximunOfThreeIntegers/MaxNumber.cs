435a5267f9b58da161e4489bb11dd7393516f6fc
Ibrahim9786 <sharuibrahim97@gmail.com>
2021-11-17 23:29:48 +0530

    MaximunOfThreeIntegers

---
+++ MaximunOfThreeIntegers/MaxNumber.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MaximunOfThreeIntegers
+{
+    public class MaxNumber
+    {
+        public static void maxNumber()
+        {
+            int num1 = 80;
+            int num2 = 100;
+            int num3 = 15;
+
+            if (num1.CompareTo(num2) > 0 && num1.CompareTo(num3) > 0 ||
+               num1.CompareTo(num2) >= 0 && num1.CompareTo(num3) > 0 ||
+                num1.CompareTo(num2) > 0 && num1.CompareTo(num3) >= 0)
+            {
+                Console.WriteLine("num 1 is greater i.e., " + num1);
+            }
+
+            if (num2.CompareTo(num1) > 0 && num2.CompareTo(num3) > 0 ||
+               num2.CompareTo(num1) >= 0 && num2.CompareTo(num3) > 0 ||
+                num2.CompareTo(num1) > 0 && num2.CompareTo(num3) >= 0)
+            {
+                Console.WriteLine("num 2 is greater i.e., " + num2);
+            }
+
+            if (num3.CompareTo(num1) > 0 && num3.CompareTo(num2) > 0 ||
+               num3.CompareTo(num1) >= 0 && num3.CompareTo(num2) > 0 ||
+                num3.CompareTo(num1) > 0 && num3.CompareTo(num2) >= 0)
+            {
+                Console.WriteLine("num 3 is greater i.e., " + num3);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MaximunOfThreeIntegers/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MaximunOfThreeIntegers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MaxNumber.maxNumber();
+
+            Console.ReadKey();
+        }
+    }
+}
