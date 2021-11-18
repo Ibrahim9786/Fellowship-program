c42a1da6acb90f75ab82e9dd922b0c8aec5c6b1a
Ibrahim9786 <sharuibrahim97@gmail.com>
2021-11-18 23:05:26 +0530

    Queue1

---
+++ Queue1/LinkedListQueue.cs
@@ -0,0 +1,46 @@
+﻿using Stack1;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Queue1
+{
+    class LinkedListQueue
+    {
+        Node head = null;
+
+        internal void Enqueue(int data)
+        {
+            Node node = new Node(data);
+            if (head == null)
+            {
+                head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+
+            }
+            Console.WriteLine("{0} inserted into queue", node.data);
+        }
+        internal void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("Queue is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+    }
+}
---
+++ Queue1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Queue1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedListQueue list = new LinkedListQueue();
+            list.Enqueue(56);
+            list.Enqueue(30);
+            list.Enqueue(70);
+            list.Display();
+
+            Console.ReadKey();
+        }
+    }
+}
