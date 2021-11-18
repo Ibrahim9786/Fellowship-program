1dc670396d99132199f73f0600083298b4ac0cb0
Ibrahim9786 <sharuibrahim97@gmail.com>
2021-11-18 23:03:04 +0530

    Stack2

---
+++ Stack2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Stack2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedListStack list = new LinkedListStack();
+
+            list.Push(70);
+            list.Push(30);
+            list.Push(56);
+            list.Display();
+            list.Peak();
+            list.Pop();
+            list.Display();
+            list.IsEmpty();
+            list.Display();
+
+            Console.ReadKey();
+        }
+    }
+}
