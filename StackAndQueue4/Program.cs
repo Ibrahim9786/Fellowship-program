9b0fb8cb699fb13c36e4895f3a22f6db33c5ca32
Ibrahim9786 <sharuibrahim97@gmail.com>
2021-11-18 23:23:33 +0530

    StackAndQueue4

---
+++ StackAndQueue4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace StackAndQueue4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList list = new LinkedList();
+
+            Console.WriteLine("Initially");
+            list.Add(56);
+            list.Add(70);
+            list.Display();
+
+            Console.WriteLine("-------------------------");
+
+
+            Console.WriteLine("After inserting 30 between 56 and 70");
+            list.InsertAtPerticularPosition(2, 30);
+            list.Display();
+
+            Console.WriteLine("After inserting 30 between 56 and 70");
+            list.Add(1, 70);
+            list.Display();
+
+            Console.ReadKey();
+        }
+    }
+}
