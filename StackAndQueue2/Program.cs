afa613bbf41cd4da0c50437efcf14e93a9721893
Ibrahim9786 <sharuibrahim97@gmail.com>
2021-11-18 23:21:05 +0530

    StackAndQueue2

---
+++ StackAndQueue2/Program.cs
@@ -0,0 +1,31 @@
+﻿using SimpleLinkedList;
+using System;
+using System.Collections.Generic;
+
+namespace StackAndQueue2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList list = new LinkedList();
+            list.Add(70);
+            list.Display();
+
+
+            Console.WriteLine("----------------");
+
+            list.Add(30);
+            list.Display();
+
+            Console.WriteLine("----------------");
+
+            list.Add(56);
+            list.Display();
+
+            Console.WriteLine("----------------");
+
+            Console.ReadKey();
+        }
+    }
+}
