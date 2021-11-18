f06a897fd426860a5159769821585bb5808046b1
Ibrahim9786 <sharuibrahim97@gmail.com>
2021-11-18 23:00:34 +0530

    Stack1

---
+++ Stack1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Stack1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedListStack list = new LinkedListStack();
+
+            list.Push(70);
+            list.Push(30);
+            list.Push(56);
+            list.Display();
+
+            Console.ReadKey();
+        }
+    }
+}
