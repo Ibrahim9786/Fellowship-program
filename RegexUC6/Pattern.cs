71cc991c2bab455e1faf04d1a96cfc186578e732
Ibrahim9786 <sharuibrahim97@gmail.com>
2021-11-27 20:56:18 +0530

    RegexUC6

---
+++ RegexUC6/Pattern.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace RegexUC6
+{
+    public class Pattern
+    {
+        public static string REGEX_PASSWORD = "^[A-Za-z]{9}$";
+
+        public bool validatePassword(string password)
+        {
+            return Regex.IsMatch(password, REGEX_PASSWORD);
+        }
+
+    }
+}
---
+++ RegexUC6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace RegexUC6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool checkvalue;
+            //in the question given atleast 1 uppercase
+            //among 9 characters, representing P as a uppercase here
+            //we can also change it to more than 1 uppercase
+            string patt = "PasswordS";
+            Pattern pattern = new Pattern();
+            checkvalue = pattern.validatePassword(patt);
+
+            if (checkvalue)
+            {
+                Console.WriteLine("Pattern matched and correct");
+            }
+            else
+            {
+                Console.WriteLine("Not matched");
+            }
+        }
+    }
+}
