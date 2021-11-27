59f4d3d0063a27525aa30b32ef179b272bb6a8d9
Ibrahim9786 <sharuibrahim97@gmail.com>
2021-11-27 21:01:36 +0530

    RegexUC8

---
+++ RegexUC8/Pattern.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace RegexUC8
+{
+    public class Pattern
+    {
+        public static string REGEX_PASSWORD = "^[A-Za-z]{9}[@]{1}[0-9]{2}$";
+
+        public bool validatePassword(string password)
+        {
+            return Regex.IsMatch(password, REGEX_PASSWORD);
+        }
+    }
+}
---
+++ RegexUC8/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace RegexUC8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool checkvalue;
+            //given as exactly 1 special character in the password
+            string patt = "Passwords@98";
+            Pattern pattern = new Pattern();
+            checkvalue = pattern.validatePassword(patt);
+
+            if (checkvalue)
+            {
+                Console.WriteLine("Pattern matched and correct");
+            }
+            else
+            {
+                Console.WriteLine("Not matched");
+            }
+        }
+    }
+}
