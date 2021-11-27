3e38e7697f6b1d94a83c8c8dd60fb0d45fa17354
Ibrahim9786 <sharuibrahim97@gmail.com>
2021-11-27 20:59:09 +0530

    RegexUC7

---
+++ RegexUC7/Pattern.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace RegexUC7
+{
+    public class Pattern
+    {
+        public static string REGEX_PASSWORD = "^[A-Za-z]{9}[0-9]{2}$";
+
+        public bool validatePassword(string password)
+        {
+            return Regex.IsMatch(password, REGEX_PASSWORD);
+        }
+    }
+}
---
+++ RegexUC7/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace RegexUC7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool checkvalue;
+            //for the same adding two numerics
+            //given as atleast one numerics to add
+            string patt = "Passwords98";
+            Pattern pattern = new Pattern();
+            checkvalue = pattern.validatePassword(patt);
+
+            if (checkvalue)
+            {
+                Console.WriteLine("Pattern matched and correct");
+            }
+            else
+            {
+                Console.WriteLine("Not matched");
+            }
+        }
+    }
+}
