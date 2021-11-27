81104412fbed33ec4647b90878d2cc56c7d18a32
Ibrahim9786 <sharuibrahim97@gmail.com>
2021-11-27 20:45:47 +0530

    RegexUc1

---
+++ RegexUc1/Pattern.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace RegexUc1
+{
+    public class Pattern
+    {
+        public static string REGEX_FULLNAME = "^[V]{1}[a-z]{4}[ ]{1}[a-z]{5}";
+
+        public bool validateFullname(string fullname)
+        {
+            return Regex.IsMatch(fullname, REGEX_FULLNAME);
+        }
+
+        
+    }
+}
---
+++ RegexUc1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace RegexUc1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool checkvalue;
+            //below given as fullname and in that first name starts with Cap
+            string patt = "Virat kohli";
+            Pattern pattern = new Pattern();
+            checkvalue = pattern.validateFullname(patt);
+
+            if (checkvalue)
+            {
+                Console.WriteLine("Pattern matched and correct");
+            }
+            else
+            {
+                Console.WriteLine("Not matched");
+            }
+        }
+    }
+}
