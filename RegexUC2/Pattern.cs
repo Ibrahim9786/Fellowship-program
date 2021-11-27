ed589fddd1bb8c62d30dd938d33685114b5c945e
Ibrahim9786 <sharuibrahim97@gmail.com>
2021-11-27 20:47:48 +0530

    RegexUC2

---
+++ RegexUC2/Pattern.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace RegexUC2
+{
+    public class Pattern
+    {
+        public static string REGEX_FULLNAME = "^[V]{1}[a-z]{4}[ ]{1}[K]{1}[a-z]{4}";
+
+        public bool validateFullname(string fullname)
+        {
+            return Regex.IsMatch(fullname, REGEX_FULLNAME);
+        }
+    }
+}
---
+++ RegexUC2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace RegexUC2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool checkvalue;
+            //below given as fullname and in that last name starts with Cap
+            //no instruction given for first name thats why it remain as Cap
+            string patt = "Virat Kohli";
+            Pattern pattern = new Pattern();
+            checkvalue = pattern.validateFullname(patt);
+
+            if (checkvalue)
+            {
+                Console.WriteLine("Pattern matched and correct");
+            }
+            else
+            {
+                Console.WriteLine("Not matched");
+            }
+        }
+    }
+}
