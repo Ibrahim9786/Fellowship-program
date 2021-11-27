7ebaa2abce70e433618ddb2ed3072845fbad4f2b
Ibrahim9786 <sharuibrahim97@gmail.com>
2021-11-27 20:49:46 +0530

    RegexUC3

---
+++ RegexUC3/Patterns.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace RegexUC3
+{
+    public class Patterns
+    {
+        public static string REGEX_EMAIL = "^[a-z]{3}[.]{1}[a-z]{3}[@]{1}[a-z]{2}[.]{1}[a-z]{2}[.]{1}[a-z]{2}$";
+
+        public bool validateEmail(string email)
+        {
+            return Regex.IsMatch(email, REGEX_EMAIL);
+        }
+    }
+}
---
+++ RegexUC3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace RegexUC3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool checkvalue;
+            string patt = "abc.xyz@bl.co.in";
+            Patterns pattern = new Patterns();
+            checkvalue = pattern.validateEmail(patt);
+
+            if (checkvalue)
+            {
+                Console.WriteLine("Pattern matched and correct");
+            }
+            else
+            {
+                Console.WriteLine("Not matched");
+            }
+        }
+    }
+}
