0564320027394e085821d5405b71405a3d745aa1
Ibrahim9786 <sharuibrahim97@gmail.com>
2021-11-27 20:54:15 +0530

    RegexUC5

---
+++ RegexUC5/Patterns.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace RegexUC5
+{
+    public class Patterns
+    {
+        public static string REGEX_PASSWORD = "^[a-z]{9}$";
+
+        public bool validatePassword(string password)
+        {
+            return Regex.IsMatch(password, REGEX_PASSWORD);
+        }
+    }
+}
---
+++ RegexUC5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace RegexUC5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool checkvalue;
+            //In the question, they only mention that minimum 8 characters
+            //here using 9 characters
+            string patt = "passwords";
+            Patterns pattern = new Patterns();
+            checkvalue = pattern.validatePassword(patt);
+
+            if (checkvalue)
+            {
+                Console.WriteLine("Pattern matched and correct");
+            }
+            else
+            {
+                Console.WriteLine("Not matched");
+            }
+        }
+    }
+}
