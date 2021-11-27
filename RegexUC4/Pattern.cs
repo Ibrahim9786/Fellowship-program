0041fae807ad9e979d113eed862b87fac20502c4
Ibrahim9786 <sharuibrahim97@gmail.com>
2021-11-27 20:52:26 +0530

    RegexUC4

---
+++ RegexUC4/Pattern.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace RegexUC4
+{
+    public class Pattern
+    {
+        public static string REGEX_MOBILE = "^[1-9]{2}[ ]{1}[0-9]{10}$";
+
+        public bool validateMobile(string mobile)
+        {
+            return Regex.IsMatch(mobile, REGEX_MOBILE);
+        }
+    }
+}
---
+++ RegexUC4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace RegexUC4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool checkvalue;
+            string patt = "91 9919819801";
+            Pattern pattern = new Pattern();
+            checkvalue = pattern.validateMobile(patt);
+
+            if (checkvalue)
+            {
+                Console.WriteLine("Pattern matched and correct");
+            }
+            else
+            {
+                Console.WriteLine("Not matched");
+            }
+        }
+    }
+}
