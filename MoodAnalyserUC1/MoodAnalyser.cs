15f88f936ada7b4d64a3921b759dbac4d4663c42
Ibrahim9786 <sharuibrahim97@gmail.com>
2021-11-29 18:17:08 +0530

    MoodAnalyserUC1

---
+++ MoodAnalyserUC1/MoodAnalyser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoodAnalyserUC1
+{
+    public class MoodAnalyser
+    {
+        public string GetMood(string mood)
+        {
+            return string.Concat("I", " ", "am", " ",  "in", " " + mood, " ", "mood");
+        }
+    }
+}
