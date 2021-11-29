640c83cf755c0fb5002df707a9220f59747e39fe
Ibrahim9786 <sharuibrahim97@gmail.com>
2021-11-29 18:19:17 +0530

    MoodAnalyserUC2

---
+++ MoodAnalyserUC2/MoodAnalyser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoodAnalyserUC2
+{
+    public class MoodAnalyser
+    {
+        public string GetMood(string mood, string s)
+        {
+            return string.Concat(mood);
+        }
+    }
+}
---
+++ MoodAnalyserUC2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MoodAnalyserUC2
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
